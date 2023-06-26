98662bf839fe2060f90045ec98cabe5ad57d3f7f
theadmin12 <89454064+theadmin12@users.noreply.github.com>
2023-06-26 10:15:32 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,19 +2,28 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors")
 const app = express();
-var dotenv = require('dotenv');
-dotenv.config();
+var compression = require('compression')
+
+app.use(compression({
+    level:6,
+    threshold:100*1000,
+    filter:(req,res)=>{
+        if(req.headers['x-no-compression']){
+            return false;
+        }
+        return compression.filter(req,res)
+    }
+}));
 
 const bollywoodRoute = require('./routes/bollywoodRoute.js');
 const newreleasesRoute = require('./routes/newreleaseRoute');
 const hollywoodRoute = require('./routes/hollywoodRoute');
 const webseriesRoute = require('./routes/webseriesRoute')
-const sliderRoute = require('./routes/sliderRoute')
 
  
 //Create DB Connection
-
-mongoose.connect(process.env.MONGO_URL).then(() => {
+const MONGODB_URI = "mongodb+srv://admin:adminmovie@cluster0.bt2gtmm.mongodb.net/movie?retryWrites=true&w=majority";
+mongoose.connect(MONGODB_URI).then(() => {
     console.log('Connected to DB');
 }).catch((err) => {
     console.log(err.message);
@@ -26,15 +35,17 @@ app.use(cors())
 
 app.use('/api/bollywoods/', bollywoodRoute); 
 app.use('/api/newreleases',newreleasesRoute)
-app.use('/api/hollywoods',hollywoodRoute)
+app.use('/api/hollywoods',hollywoodRoute) 
 app.use('/api/webseries',webseriesRoute)
-app.use('/api/sliders',sliderRoute)
-
 
  
+ 
+app.get("/", (req, res) => {
+    res.send("Server in running!");
+});
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
-});
+});  
